b53c8ee4687edbeebf69874b5165d0732cf61ab2
Pedro Oliveira <pedro_oliver13@hotmail.com>
2020-12-11 13:57:08 -0300

    Update pistasRoute.ts

--- src/routes/pistasRoutes/pistasRoute.ts
+++ src/routes/pistasRoutes/pistasRoute.ts
@@ -8,7 +8,7 @@ routes.get('/', pistaController.index);
 routes.get('/:id', pistaController.show);
 routes.post('/', pistaController.post);
 routes.put('/edit/:id', pistaController.put);
-routes.delete('/:id', pistaController.delete);
+routes.delete('/edit/:id', pistaController.delete);
 
 // Criando historico 
 routes.post('/:id/historicos', historicoController.post);
